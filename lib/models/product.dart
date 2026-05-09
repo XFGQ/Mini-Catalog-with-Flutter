92b68d6cd4cca6c60e840843b5553950abb02e90
XFGQ <furkanduman1925@gmail.com>
2026-05-09 17:40:41 +0200

    feat: product file added

---
+++ lib/models/product.dart
@@ -0,0 +1,13 @@
+class Product {
+  final String name;
+  final String price;
+  final String description;
+  final String imageUrl; //temporarly we gonna use image from google
+
+  Product({
+    required this.name,
+    required this.price,
+    required this.description,
+    required this.imageUrl,
+  });
+}
